91f1c53fb633a9a2d61b96b6fa46040318e67a04
JanosSarkoezi <j.sarkoezi@googlemail.com>
2022-09-06 19:04:32 +0200

    asdf

--- pool/nvim/.config/nvimNew/lua/user/colorscheme.lua
+++ pool/nvim/.config/nvimNew/lua/user/colorscheme.lua
@@ -2,6 +2,26 @@
 -- vim.g.tokyonight_style = "night"
 local colorscheme = "carbonfox"
 
+local palettes = {
+  carbonfox = {
+    -- A palette also defines the following:
+    --   bg0, bg1, bg2, bg3, bg4, fg0, fg1, fg2, fg3, sel0, sel1, comment
+    --
+    -- These are the different foreground and background shades used by the theme.
+    -- The base bg and fg is 1, 0 is normally the dark alternative. The others are
+    -- incrementally lighter versions.
+    -- bg1 = "#2e3440",
+
+    -- sel is different types of selection colors.
+    sel0 = "#264F78", -- Popup bg, visual selection bg
+    sel1 = "#4f6074", -- Popup sel bg, search bg
+
+    -- comment is the definition of the comment color.
+    comment = "#60728a",
+  },
+}
+require("nightfox").setup({ palettes = palettes })
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
